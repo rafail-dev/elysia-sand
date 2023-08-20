393cbea2125953ecc9469a2f3fca40625e3f13a4
rafail-dev <rafail.dev@gmail.com>
2023-08-20 17:44:16 +0000

    Via typeof

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,31 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+type ExtractPath<T> = T extends Elysia<infer P, infer I> ? P : never;
+type ExtractAndVerifyInstance<T, R> = T extends Elysia<infer P, infer I>
+  ? I extends R
+  ? I
+  : never
+  : never;
+
+const app = new Elysia()
+  .state("mykey", "hello")
+  .state("another", "hello")
+
+  .group("mypath", (app) => {
+    type MyRequirements = {
+      store: {
+        mykey: string;
+      };
+    };
+
+    type PathType = ExtractPath<typeof app>;
+    type InstanceType = ExtractAndVerifyInstance<typeof app, MyRequirements>;
+
+    const verified: Elysia<PathType, InstanceType> = app;
+
+    return app;
+  })
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
